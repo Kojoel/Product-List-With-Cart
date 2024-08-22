db98162715a173498c642d6adabc8f8622ac3961
Kojoel <nanaabruquah1s@gmail.com>
2024-08-22 03:43:15 +0000

    Cart component Styled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { DessertsComponent } from './components/desserts/desserts.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductCardComponent, DessertsComponent],
+  imports: [RouterOutlet, ProductCardComponent, DessertsComponent, CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
