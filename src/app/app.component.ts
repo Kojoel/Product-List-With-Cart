0c41a841beede7ac3c38060b1bc7a2e5c06dd697
Kojoel <nanaabruquah1s@gmail.com>
2024-08-27 12:12:01 +0000

    Modal component implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,18 @@ import { DessertsComponent } from './components/desserts/desserts.component';
 import { CartComponent } from './components/cart/cart.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ModalComponent } from "./components/modal/modal.component";
+import { CartService } from './services/cart.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductCardComponent, DessertsComponent, CartComponent, HttpClientModule, ModalComponent],
+  imports: [RouterOutlet, ProductCardComponent, DessertsComponent, CartComponent, HttpClientModule, ModalComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Product-List-with-Cart';
+
+  constructor(public cartService: CartService){}
+
 }
--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { ProductItem } from '../../model/productItem';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
 export class ModalComponent {
   constructor(public cartService: CartService) {}
 
+  cartItems: ProductItem[] = [];
+
   hideModal: boolean =false;
+
+  ngOnInit(): void {
+    this.cartService.cartItems$.subscribe(items => {
+      this.cartItems = items;
+    });
+  }
 
   startNewOrder() {
       this.cartService.confirmOrder = this.hideModal;
